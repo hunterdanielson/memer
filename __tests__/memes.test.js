c36c89bdb972ad516db12035217f990965b73d83
Hunter Danielson <hunterleedanielson@gmail.com>
2020-06-30 14:25:10 -0700

    get all and get by id tests pass

--- __tests__/memes.test.js
+++ __tests__/memes.test.js
@@ -5,6 +5,9 @@ const connect = require('../lib/utils/connect');
 
 const request = require('supertest');
 const app = require('../lib/app');
+const Meme = require('../lib/models/Meme');
+const seed = require('../lib/utils/seed');
+const { prepare } = require('../lib/utils/prepare');
 
 describe('memer routes', () => {
   beforeAll(async() => {
@@ -14,6 +17,9 @@ describe('memer routes', () => {
 
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
+  });
+  beforeEach(() => {
+    return seed();
   });
 
   afterAll(async() => {
@@ -38,5 +44,19 @@ describe('memer routes', () => {
           __v: 0
         });
       });
+  });
+
+  it('will get all memes via GET', async() => {
+    const allMemes = prepare(await Meme.find());
+    return request(app)
+      .get('/api/v1/memes')
+      .then(res => expect(res.body).toEqual(allMemes));
+  });
+
+  it('will get a meme by id via GET', async() => {
+    const meme = prepare(await Meme.findOne());
+    return request(app)
+      .get(`/api/v1/memes/${meme._id}`)
+      .then(res => expect(res.body).toEqual(meme));
   });
 });
